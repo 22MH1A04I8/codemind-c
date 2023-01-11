6495d1889136fa71217b49ed8d3588bd2c7423a1
Ratna Prasanna Sunkara <119718895+22MH1A04I8@users.noreply.github.com>
2023-01-11 17:19:35 +0530

    #include<stdio.h> int main() {     int num,rev,rem;     scanf("%d",&num);     for(rev=0;num!=0;num=num/10)     {         rem=num%10;         rev=rev*10+rem;
        }
        printf("%d",rev);
        
    }

---
+++ reverse_of_a_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num,rev,rem;
+    scanf("%d",&num);
+    for(rev=0;num!=0;num=num/10)
+    {
+        rem=num%10;
+        rev=rev*10+rem;
+        
+    }
+    printf("%d",rev);
+    
+}
